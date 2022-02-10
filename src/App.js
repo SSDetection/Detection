7791496b6ece235cd91f976ddcdf0d87abf7b873
robertsonbrinker <robertsonbrinker@gmail.com>
2022-02-10 13:00:32 -0500

    youtube look

--- src/App.js
+++ src/App.js
@@ -24,9 +24,9 @@ function App() {
     { name: '@stafford', score: 9 },
   ];
   
-  var width = 900;
-  var height = 450;
-  var margin = { top: 50, bottom: 50, left: 1, right: 1};
+  var width = 200;
+  var height = 200;
+  var margin = { top: 20, bottom: 20, left: 1, right: 1};
   
   var svg = d3.select('#d3-container')
     .append('svg')
@@ -59,13 +59,13 @@ function App() {
   function yAxis(g) {
     g.attr("transform", `translate(${margin.left}, 0)`)
       .call(d3.axisLeft(y).ticks(null, data.format))
-      .attr("font-size", '20px')
+      .attr("font-size", '6px')
   }
   
   function xAxis(g) {
     g.attr("transform", `translate(0,${height - margin.bottom})`)
       .call(d3.axisBottom(x).tickFormat(i => data[i].name))
-      .attr("font-size", '20px')
+      .attr("font-size", '6px')
   }
   
   svg.append("g").call(xAxis);
@@ -77,9 +77,9 @@ function App() {
 
   // Step 3
   var svg = d3.select("svg");
-  var width = 500;
-  var height = 400;
-  var radius = 200;
+  var width = 200;
+  var height = 200;
+  var radius = 100;
 
   // Step 1        
   var data = [{name: "Gun", share: 75}, 
@@ -157,56 +157,185 @@ function App() {
   return (
     <div >
 
-      <header className="App-header">
+      <header className="App-header ">
 
-        <form onSubmit={addTodo} align="center">
+        <form onSubmit={addTodo} class="center" align="center">
           <input type="text" placeholder="Enter name here" ref={todoText} />
           <input type="submit" value="Search" />
         </form>
 
-        <div class="carousel">
-          <Carousel thumbWidth={thumbwidth}>
-            <div>
-              <img src="https://picsum.photos/800/401/?random" />
-              <p className="legend">Pic 1</p>
-            </div>
-            <div>
-              <img src="https://picsum.photos/800/402/?random" />
-              <p className="legend">Pic 2</p>
-            </div>
-            <div>
-              <img src="https://picsum.photos/800/400/?random" />
-              <p className="legend">Pic 3</p>
-            </div>
-          </Carousel>
-        </div>
-
-        <div className="row">
-          <div className="column">
-            <img src="https://picsum.photos/300/100/?random" />
-            <img src="https://picsum.photos/300/200/?random" />
-            <img src="https://picsum.photos/300/300/?random" />
-            <img src="https://picsum.photos/300/400/?random" />
-            <img src="https://picsum.photos/300/301/?random" />
-            <img src="https://picsum.photos/300/201/?random" />
-            <img src="https://picsum.photos/300/101/?random" />
-          </div>
-          <div className="column">
-            <img src="https://picsum.photos/300/104/?random" />
-            <img src="https://picsum.photos/300/204/?random" />
-            <img src="https://picsum.photos/300/304/?random" />
-            <img src="https://picsum.photos/300/402/?random" />
-            <img src="https://picsum.photos/300/302/?random" />
-            <img src="https://picsum.photos/300/202/?random" />
-            <img src="https://picsum.photos/300/102/?random" />
-          </div>
-        </div>
-
-        <ul>
-          {todos.map(todo => (<li key={todo}>{todo}</li>))}  
-        </ul>
+        <form action="/action_page.php" class="sidebyside right" align="right">
+            <label for="cars">Sort Posts by</label>
+              <select name="cars" id="cars">
+                <optgroup label="Sorting Options">
+                  <option value="Latest Posts">Latest Posts</option>
+                  <option value="Score">Score</option>
+                </optgroup>
+              </select>
+              <br></br>
+              <input type="submit" value="Sort"></input>
+          </form>
         
       </header>
+
+      <div class="sidebyside body">
+        {/* Side Bar */}
+        <div class="sidebar">
+          <h1>@username</h1>
+          <h1 class="orange">
+            Interest Level: Medium
+          </h1>
+          <div class="sidebyside font10 orange">
+            <ul>
+              <li>knives</li>
+              <li>guns</li>
+              <li>threatening speech</li>
+            </ul>
+            <ul>
+              <li>1 potential</li>
+              <li>2 potential</li>
+              <li>5 potential</li>
+            </ul>
+          </div>
+
+
+          <h6>Percentage of Gun Posts for @Shooterboy</h6>
+          <svg width={width} height={height}></svg> 
+
+        
+          <div id="d3-container" class="redback">
+          <h6>Percentage of Posts with Guns</h6>
+        
+        </div>
+        </div>
+        {/* Main */}
+        <div class="display">
+
+          <div class="sidebyside">
+            <div class="result centercontent">
+              <p class="analysis"><span class="green">clear</span></p>
+              <img src="https://picsum.photos/100/100/?random" class="center"/>
+              <p class="comment">All your base are belong to us</p>
+            </div>
+
+            <div class="result centercontent">
+              <p class="analysis"><span class="red">language</span></p>
+              <img src="https://picsum.photos/100/99/?random" class="center"/>
+              <p class="comment">spoiler alert thanos <span class="red">dies</span></p>
+            </div>
+
+            <div class="result centercontent">
+              <p class="analysis"><span class="red">gun</span></p>
+              <img src="https://picsum.photos/100/101/?random" class="center"/>
+              <p class="comment">at the range</p>
+            </div>
+
+            <div class="result centercontent">
+              <p class="analysis"><span class="red">knife</span></p>
+              <img src="https://picsum.photos/100/102/?random" class="center"/>
+              <p class="comment">my new kunai</p>
+            </div>
+          </div>
+
+          <div class="sidebyside">
+            <div class="result centercontent">
+              <p class="analysis"><span class="green">clear</span></p>
+              <img src="https://picsum.photos/101/101/?random" class="center"/>
+              <p class="comment">no u</p>
+            </div>
+
+            <div class="result centercontent">
+              <p class="analysis"><span class="green">clear</span></p>
+              <img src="https://picsum.photos/101/100/?random" class="center"/>
+              <p class="comment">who else misses dan the train man</p>
+            </div>
+
+            <div class="result centercontent">
+              <p class="analysis"><span class="red">language gun</span></p>
+              <img src="https://picsum.photos/101/99/?random" class="center"/>
+              <p class="comment">suns out guns out or I get <span class="red">hangry</span></p>
+            </div>
+
+            <div class="result centercontent">
+              <p class="analysis"><span class="green">clear</span></p>
+              <img src="https://picsum.photos/101/102/?random" class="center"/>
+              <p class="comment">nice pic huh</p>
+            </div>
+          </div>
+
+          <div class="sidebyside">
+            <div class="result centercontent">
+              <p class="analysis"><span class="green">clear</span></p>
+              <img src="https://picsum.photos/99/100/?random" class="center"/>
+              <p class="comment">example</p>
+            </div>
+
+            <div class="result centercontent">
+              <p class="analysis"><span class="red">language</span></p>
+              <img src="https://picsum.photos/99/101/?random" class="center"/>
+              <p class="comment">another example <span class="red">dies</span></p>
+            </div>
+
+            <div class="result centercontent">
+              <p class="analysis"><span class="red">language</span></p>
+              <img src="https://picsum.photos/99/102/?random" class="center"/>
+              <p class="comment">happy <span class="red">dies</span> in the movies</p>
+            </div>
+
+            <div class="result centercontent">
+              <p class="analysis"><span class="red">language</span></p>
+              <img src="https://picsum.photos/99/99/?random" class="center"/>
+              <p class="comment">another excusse to say<span class="red">dies</span></p>
+            </div>
+          </div>
+          
+          
+
+        </div>
+      </div>
+
+
+      <div class="carousel">
+        <Carousel thumbWidth={thumbwidth}>
+          <div>
+            <img src="https://picsum.photos/800/401/?random" />
+            <p className="legend">Pic 1</p>
+          </div>
+          <div>
+            <img src="https://picsum.photos/800/402/?random" />
+            <p className="legend">Pic 2</p>
+          </div>
+          <div>
+            <img src="https://picsum.photos/800/400/?random" />
+            <p className="legend">Pic 3</p>
+          </div>
+        </Carousel>
+      </div>
+
+      <div className="row">
+        <div className="column">
+          <img src="https://picsum.photos/300/100/?random" />
+          <img src="https://picsum.photos/300/200/?random" />
+          <img src="https://picsum.photos/300/300/?random" />
+          <img src="https://picsum.photos/300/400/?random" />
+          <img src="https://picsum.photos/300/301/?random" />
+          <img src="https://picsum.photos/300/201/?random" />
+          <img src="https://picsum.photos/300/101/?random" />
+        </div>
+        <div className="column">
+          <img src="https://picsum.photos/300/104/?random" />
+          <img src="https://picsum.photos/300/204/?random" />
+          <img src="https://picsum.photos/300/304/?random" />
+          <img src="https://picsum.photos/300/402/?random" />
+          <img src="https://picsum.photos/300/302/?random" />
+          <img src="https://picsum.photos/300/202/?random" />
+          <img src="https://picsum.photos/300/102/?random" />
+        </div>
+      </div>
+
+      <ul>
+        {todos.map(todo => (<li key={todo}>{todo}</li>))}  
+      </ul>
 
       <div class="summary">
         <div>
@@ -229,19 +358,7 @@ function App() {
           <img src="https://picsum.photos/300/299/?random"/>
           <p># of posts vs time</p>
         </div>
-        <div>
-          <form action="/action_page.php">
-            <label for="cars">Sort Posts by</label>
-              <select name="cars" id="cars">
-                <optgroup label="Sorting Options">
-                  <option value="Latest Posts">Latest Posts</option>
-                  <option value="Score">Score</option>
-                </optgroup>
-              </select>
-              <br></br>
-              <input type="submit" value="Sort"></input>
-          </form>
-        </div>
+        
       </div>
 
       <div class="summary">
@@ -307,14 +424,10 @@ function App() {
         
        
       </div>*/}
-      <h2>Percentage of Gun Posts for @Shooterboy</h2>
-      <svg width={width} height={height}></svg> 
+     
 
       
-        <h1>Percentage of Posts with Guns</h1>
-        <div id="d3-container">
-          <svg></svg>
-        </div>
+        
      
 
     </div>
