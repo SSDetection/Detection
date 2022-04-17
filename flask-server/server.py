c32babe8ef181832b7f8fd0eb123528a09edfa5c
Endrit <endritzenuni112@gmail.com>
2022-04-16 20:26:26 -0400

    Fixed Test function for API call

--- flask-server/server.py
+++ flask-server/server.py
@@ -202,6 +202,7 @@ def download_profile(profileName):
 
 
 app = Flask(__name__)
+@app.route("/test", methods = ['GET'])
 def testfunc():
     results = {'Data': [{'Image': 'zuni.115/zuni.1150.jpg', 'Accuracy': '49.86', 'Date': 'JANUARY 28', 'Caption': 'Planning on adding some attachments later #palmettostatearmory #guns #cz #9mm #ar #ar15 #czp07 #556nato #leapoldoptics #rifles #suppressor'}, {'Image': 'zuni.115\\zuni.1151.jpg', 'Accuracy': '43.35', 'Date': 'JANUARY 30', 'Caption': 'Finally Bought a Sig red dot for my AR from Glick-Twins. #palmettostatearmory #guns #cz #9mm #ar #ar15 #czp07 #556nato #leapoldoptics #rifles #suppressor #glicktwins'}, {'Image': 'zuni.115\\zuni.1152.jpg', 'Accuracy': '48.72', 'Date': 'JANUARY 28', 'Caption': 'Got a flashlight that my big bro gave me. Bout to attach this to my AR. #palmettostatearmory #guns #cz #9mm #ar #ar15 #czp07 #556nato #leapoldoptics #rifles #suppressor'}]}
     return results
