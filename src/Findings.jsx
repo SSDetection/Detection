27555db0b6f0007538fcda046a83ae689338f000
robertsonbrinker <robertsonbrinker@gmail.com>
2022-04-18 19:28:12 -0400

    added nav

---
+++ src/Findings.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+function Findings() {
+  return (
+    <div className="about">
+      <div class="container">
+        <div class="row align-items-center my-5">
+          <div class="col-lg-7">
+            <img
+              class="img-fluid rounded mb-4 mb-lg-0"
+              src="http://placehold.it/900x400"
+              alt=""
+            />
+          </div>
+          <div class="col-lg-5">
+            <h1 class="font-weight-light">About</h1>
+            <p>
+              over arching findings from all things such as
+            </p>
+            <p>
+            86% of participants were willing to give up privacy
+            </p>
+            <p>
+            most of participants designed something having to do with scrapping social media
+            </p>
+            <p>
+            Almost no one brought up...
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Findings;
