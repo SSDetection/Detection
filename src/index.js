eaeb9ba05f8d4ee7b3bdb193613d358192a9ed43
Endritz <55924994+Endritz@users.noreply.github.com>
2022-04-18 20:42:17 -0400

    Endrit experimental to master (#9)
    * graphs
    
    * youtube look
    
    * partial scrapper implementation
    
    * loading instagram pics
    
    * custom dimesnions
    
    * Playing around
    
    * Figured some backend stuff
    
    * IM ONTO SOMETHING
    
    * Post request is fixed
    
    * Firebase Static Hosting
    
    Fix was to change the package.json Homepage element to the relevant URL
    
    * added firebase storage
    
    * Is able to display through the entire json
    
    * Bigger dictionary
    
    More objects
    
    * pictures visible
    
    * Fixed git ignore i think
    
    * Update .gitignore
    
    * Delete ServiceAccountKey.json
    
    this was kind of important to put on github
    
    * SCRAPER WORKS WITH FRONTEND!!!
    
    * pic formatting
    
    * added Delete
    
    * added captions
    
    * text appearance
    
    * Added functional ML model
    
    Works with JSON
    
    * Merge complete?
    
    * Endrit experimental Merge with React Dev Unstable (#4)
    
    * Playing around
    
    * Figured some backend stuff
    
    * IM ONTO SOMETHING
    
    * Post request is fixed
    
    * Is able to display through the entire json
    
    * Bigger dictionary
    
    More objects
    
    * SCRAPER WORKS WITH FRONTEND!!!
    
    * Added functional ML model
    
    Works with JSON
    
    * Merge complete?
    
    * Refactored Code
    
    * Gitignore is not working what
    
    * Update server.py
    
    * Fixed Test function for API call
    
    * fixed white space at bottom
    
    * New Model and site finalization
    
    * added nav
    
    * logo and coloring
    
    * recoloring and nav bar looks
    
    Co-authored-by: robertsonbrinker <robertsonbrinker@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,14 +2,25 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Archive from './Archive'
+import Findings from './Findings'
+import Navigation from './Navigation';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <Navigation />
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/archive" element={<Archive />} />
+      <Route path="/findings" element={<Findings />} />
+    </Routes>
+  </Router>,
   document.getElementById('root')
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
